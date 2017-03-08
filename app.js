4ef59c50162c4e15852ead162fe185396a63775a
Pham Trong Tri <trongtri160592@yahoo.com>
2017-03-09 00:01:11 +0700

    add static file templates

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -20,10 +21,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/js', express.static(__dirname + '/node_modules/particle-api-js/dist')); // redirect particle JS
+
+// define routes
 app.use('/', index);
 app.use('/users', users);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/about.js
@@ -0,0 +1,13 @@
+/**
+ * Created by tri on 27/02/2017.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render('about', {title: 'About'});
+});
+
+module.exports = router;
